5dac3166567a157fb1a8ea2fa6738e3f7a4acb47
K Fraser <kfraser@college.harvard.edu>
2017-06-12 11:01:31 -0400

    new file:   data_import_modified.py 	new file:   image_generator.py

---
+++ python/data_import_modified.py
@@ -0,0 +1,68 @@
+# Modified by Katie Fraser, Harvard, 2017
+# From code written by Patrick Komiske, MIT, 2017
+# Function for importing jet images from multiple .npz files into one numpy
+# array. Tries to be efficient by allocating all necessary memory at the 
+# beginning. Can also read in events assuming a csv style file format with
+# an extra newline per event, particles listed one per line, comments 
+# beginning with a '#', and lines denoting overal jet properties beginning
+# with 'Event #, [jet_rap], [jet_phi], [jet_pt]'.
+
+
+import numpy as np
+import csv
+import os
+import heppy
+
+def data_import(data_type, seed_range, seed_number = 1, particle_type = '', prefix = '', path = '', 
+                nevents = 10000, img_size = 33, nb_chan = 1):
+
+    """ Imports data produced by the Events.cc script into python. Note that both
+    gluon and quark files must be present for the desired seed range. The gluons 
+    are always listed before the quarks. We assume a constant number of events
+    per file.
+
+    data_type: Either 'jetimage' or 'event'. These
+               respectively return a numpy array of jet images and a list of events with their particle constituents and a separate
+               list of the overall jet four-vectors.
+    seed_range: a list or other iterable object containing the seeds for each file
+    path: path to directory with the files. Defaults to '../events' and 
+          '../images' for the type possible data_types.
+    nevents: number of events per file. Note that this needs to be constant across
+             the files.
+    img_size: the image size of the jet images.
+    channels: the channels of the jet image to return.
+    """
+
+    assert data_type in ['jetimage', 'event'], 'data_type not recognized'
+
+    if len(path) == 0:
+        path = '../events' if data_type == 'event' else '../images'
+
+    if data_type == 'jetimage':
+        gluon_string = prefix + 'gluon-jetimage-seed{}_33x33images_' + str(nb_chan) + 'chan.npz'
+        quark_string = prefix + 'quark-jetimage-seed{}_33x33images_' + str(nb_chan) + 'chan.npz'
+        return heppy.load_images([gluon_string.format(x) for x in seed_range],
+                                 [quark_string.format(x) for x in seed_range],
+                                 nevents * len(seed_range), nevents * len(seed_range), 
+                                 nb_chan = nb_chan)
+    elif data_type == 'event':
+        jets = []
+        jet_tots = []
+
+        filename = prefix + particle_type + '-event-seed' + str(seed_number)
+        with open(os.path.join(path, filename + '.txt'), 'r') as fh:
+            reader = csv.reader(fh)
+            jet = []
+            for row in reader:
+                if len(row) > 0 and '#' in row[0]:
+                    continue
+                if len(row) > 0 and 'Event' in row[0]:
+                     jet_tots.append(list(map(float, row[1:])))
+                elif len(row) == 0:
+                    jets.append(np.asarray(jet))
+                    jet = []
+                else:
+                    jet.append(list(map(float, row)))
+
+    
+        return jets, jet_tots
---
+++ python/image_generator.py
@@ -0,0 +1,29 @@
+# Katie Fraser, Harvard, 2017
+# Adapted from code by Patrick Komiske, MIT, 2017
+#
+# Reads in data from event/particle information, 
+# create the jet images, and save them to file.
+
+import heppy
+from data_import_modified import data_import
+import numpy as np
+import math
+
+# specify number of files to use
+n_files = 10
+
+#Loop through energies
+for energy in ['1000GEV']: 
+    # Loop through quark and gluon events
+    for particle_type in ['gluon', 'quark']:
+        # Loop through seed number
+        for seed_number in range(1,1 + n_files):
+            # import the jets
+            jets, jet_tots = data_import('event', range(1,1 + n_files), seed_number, particle_type, energy)
+
+            # form the jet images
+            jet_images = np.asarray([heppy.pixelate(jet, nb_chan = 3,img_width = 0.8) for jet in jets])
+ 
+            # save the jet images to file
+            filename = energy + particle_type + '-jetimage-seed' + str(seed_number)
+            heppy.write_images_to_file(filename, jet_images)
