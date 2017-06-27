e46567c54a0bb020756adb66b2d1d3a2431898ee
K Fraser <kfraser@college.harvard.edu>
2017-06-27 18:40:11 -0400

    modified:   generator.py

--- python/keras_helpers/generator.py
+++ python/keras_helpers/generator.py
@@ -13,20 +13,15 @@ def generator(hps, X_train, Y_train):
             X_aug, Y_aug = apply_jitter(X_batch,Y_batch)
             yield X_aug, Y_aug
     if hps['nb_channels'] == 2:
-        X_batch = np.zeros((hps['batch_size'],hps['nb_channels'],hps['img_size'],hps['img_size']))
-        Y_batch = np.zeros((hps['batch_size'],hps['nb_channels']))
         counter = 0
         while True:
-            m = 0
-            for i in range(counter, counter + hps['batch_size']):
-                for j in range(hps['nb_channels']):
-                    for k in range(hps['img_size']):
-                        for l in range(hps['img_size']):
-                            X_batch[m,j,k,l] = X_train[i % len(X_train),j,k,l]
-                    Y_batch[m,j] = Y_train[i % len(X_train),j]
-                m += 1
-                counter += hps['batch_size']
-                counter = counter % len(X_train)
+            if (counter + hps['batch_size']) < len(X_train):
+                X_batch = np.stack([X_train[i,:,:,:] for i in range(counter, counter + hps['batch_size'])])
+                Y_batch = np.stack([Y_train[i,:] for i in range(counter, counter + hps['batch_size'])])
+            else:
+                X_batch = np.stack(([X_train[i,:,:,:] for i in range(counter, len(X_train))],[X_train[i,:,:,:] for i in range(0, (counter + hps['batch_size'])%len(X_train))]))
+                Y_batch = np.stack(([Y_train[i,:] for i in range(counter, len(X_train))],[X_train[i,:] for i in range(0, (counter + hps['batch_size'])%len(X_train))]))
             X_aug, Y_aug = apply_jitter(X_batch,Y_batch)
             yield X_aug, Y_aug
+
 
